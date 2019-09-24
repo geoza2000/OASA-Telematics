b32cfeca5cc6ec372047059ddeb1572821c6de9c
George Zaimis <geoza2000@gmail.com>
2019-09-25 01:45:26 +0300

    getLines

--- controllers/lines/lines.js
+++ controllers/lines/lines.js
@@ -1,8 +1,24 @@
 //Services
 const helper = require('../../services/helper')
 
-module.exports = () => {
+module.exports = (req, res) => {
+
+    var lines = []
     
-    
+    helper.get('http://telematics.oasa.gr/api', {"act": "webGetLines"}).then(response =>{
+        response.forEach(line => {
+            lines.push({
+                code: line["LineCode"],
+                id: line["LineID"],
+                desc: {
+                    el: line["LineDescr"],
+                    en: line["LineDescrEng"]
+                }
+            })
+        });
+        res.send(lines)
+    }).catch(err => {
+        res.status(500).send(err)
+    })
 
 }
