e27b6952d39cc12b653d1f2d317aa94b2608792d
George Zaimis <geoza2000@gmail.com>
2019-09-25 22:58:46 +0300

    Implement caching mechanism

--- controllers/lines/lines.js
+++ controllers/lines/lines.js
@@ -1,24 +1,48 @@
 //Services
 const helper = require('../../services/helper')
+const cache = require('../../services/cache')
+
+//Const
+const CACHE_KEY = "lines"
+const CACHE_TTL = 43200
 
 module.exports = (req, res) => {
 
-    var lines = []
-    
-    helper.get('http://telematics.oasa.gr/api', {"act": "webGetLines"}).then(response =>{
-        response.forEach(line => {
-            lines.push({
-                code: line["LineCode"],
-                id: line["LineID"],
-                desc: {
-                    el: line["LineDescr"],
-                    en: line["LineDescrEng"]
-                }
-            })
-        });
-        res.send({data: lines})
-    }).catch(err => {
-        res.status(500).send({error: err})
-    })
+    const cached_data = cache.get(CACHE_KEY)
+
+    if (cached_data) {
+
+        res.send(cached_data)
+
+    } else {
+
+        var lines = []
+
+        helper.get('http://telematics.oasa.gr/api', {"act": "webGetLines"}).then(response => {
+            response.forEach(line => {
+                lines.push({
+                    code: line["LineCode"],
+                    id: line["LineID"],
+                    desc: {
+                        el: line["LineDescr"],
+                        en: line["LineDescrEng"]
+                    }
+                })
+            });
+
+            const data = {
+                data: lines
+            }
+            cache.set(CACHE_KEY, data, CACHE_TTL)
+
+            res.send(data)
+        }).catch(err => {
+
+            const error = {
+                error: err
+            }
+            res.status(500).send(error)
+        })
+    }
 
 }
--- controllers/lines/masterlines.js
+++ controllers/lines/masterlines.js
@@ -1,27 +1,46 @@
 //Services
 const helper = require('../../services/helper')
+const cache = require('../../services/cache')
+
+//Const
+const CACHE_KEY = "masterlines"
+const CACHE_TTL = 43200
 
 module.exports = (req, res) => {
 
-    var masterlines = []
+    const cached_data = cache.get(CACHE_KEY)
 
-    helper.get('http://telematics.oasa.gr/api', {"act": "webGetMasterLines"}).then(response => {
-        response.forEach(masterline => {
-            masterlines.push({
-                code: masterline["ml_code"],
-                id: masterline["ml_id"],
-                parent_line: masterline["line_code"],
-                schedule: masterline["sdc_code"],
-                desc: {
-                    el: masterline["ml_descr"],
-                    en: masterline["ml_descr_eng"]
-                }
-            })
-        });
+    if (cached_data) {
 
-        res.send({data: masterlines})
-    }).catch(err => {
-        res.status(500).send({error: err})
-    })
+        res.send(cached_data)
+
+    } else {
+
+        var masterlines = []
+
+        helper.get('http://telematics.oasa.gr/api', {"act": "webGetMasterLines"}).then(response => {
+            response.forEach(masterline => {
+                masterlines.push({
+                    code: masterline["ml_code"],
+                    id: masterline["ml_id"],
+                    parent_line: masterline["line_code"],
+                    schedule: masterline["sdc_code"],
+                    desc: {
+                        el: masterline["ml_descr"],
+                        en: masterline["ml_descr_eng"]
+                    }
+                })
+            });
+
+            const data = {
+                data: masterlines
+            }
+            cache.set(CACHE_KEY, data, CACHE_TTL)
+
+            res.send(data)
+        }).catch(err => {
+            res.status(500).send({error: err})
+        })
+    }
 
 }
