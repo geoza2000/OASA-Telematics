0e88fc49243f4f82786b318b88cba60d60ad4b88
George Zaimis <geoza2000@gmail.com>
2019-10-05 00:40:28 +0300

    Add errors

--- services/error.js
+++ services/error.js
@@ -1,5 +1,9 @@
-exports.notFound = function(res){ 
+exports.notFound = function(res) { 
     res.status(404).send({error: {code: "404", message: "The requested data didn't returned any results."}})
+}
+
+exports.missingFields = function(res) {
+    res.status(400).send({error: {code: "400", message: "Missing fields, please check the api documentation."}})
 }
 
 exports.internal = function(res, error) {
