95ecde2ae57fae0f436990b8ce2ba6bd6ce25329
George Zaimis <geoza2000@gmail.com>
2019-10-05 00:40:56 +0300

    Add endpoint to find nerby stops

---
+++ controllers/stops/nearby.js
@@ -0,0 +1,50 @@
+//Services
+const helper = require('../../services/helper')
+const error = require('../../services/error')
+
+module.exports = (req, res) => {
+    //Const
+    const LAT = req.query["lat"]
+    const LNG = req.query["lng"]
+
+    if (LAT && LNG) {
+
+        var stops = []
+
+        helper.get('http://telematics.oasa.gr/api', {"act": "getClosestStops", "p1": LAT, "p2": LNG}).then(response => {
+            
+            if (response.length > 0) {
+                response.forEach(stop => {
+                    stops.push({
+                        code: stop["StopCode"],
+                        id: stop["StopID"],
+                        desc: {
+                            el: stop["StopDescr"],
+                            en: stop["StopDescrEng"]
+                        },
+                        location: {
+                            heading: stop["StopHeading"],
+                            street: {
+                                el: stop["StopStreet"],
+                                en: stop["StopStreetEng"]
+                            },coords: {
+                                lat: stop["StopLat"],
+                                lng: stop["StopLng"]
+                            }
+                        },
+                        distance: stop["distance"]
+                    })
+                });
+
+                var data = {
+                    data: stops
+                }
+                res.send(data)
+            }
+            error.notFound(res)  
+
+        }).catch(err => error.internal(err))
+    } else {
+        error.missingFields(res)
+    }
+}
--- routes.js
+++ routes.js
@@ -6,6 +6,8 @@ const lines = require('./controllers/lines/lines')
 const routes = require('./controllers/lines/routes')
 const masterlines = require('./controllers/lines/masterlines')
 
+const nearby = require('./controllers/stops/nearby')
+
 router.get('/lines', lines)
 router.get('/lines/:line', lines)
 router.get('/lines/:line/routes', routes)
@@ -13,5 +15,9 @@ router.get('/lines/:line/routes/:route', routes)
 
 router.get('/masterlines', masterlines)
 router.get('/masterlines/:masterline', masterlines)
+
+
+router.get('/stops/nearby', nearby)
+
 
 module.exports = router
